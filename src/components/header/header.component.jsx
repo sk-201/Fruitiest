a753736a516bf3cd3ed7fce29380a5d1a997336b
Salar Khan <salarkhan586@gmail.com>
2021-09-08 11:50:04 +0530

    Started out app with some initial data

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import {Link} from 'react-router-dom';
+import './header.styles.css';
+const Header=()=>(
+    <div className="header">
+        <div className='options'>
+            <div className='option'>
+                <big>Fruitiest</big>
+                <big>Shop</big>
+                <big>Contact</big>
+               
+                
+            </div>
+
+        </div>
+    </div>
+)
+export default Header;
